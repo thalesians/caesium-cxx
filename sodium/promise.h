1a2202a7fb520c92a04238ee48607e0e82b0aea5
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2016-06-29 11:11:25 +1200

    Add promise.h - it leaks memory, and this is not the fault of promise itself, but of the Sodium implementation. That's the next job.

---
+++ sodium/promise.h
@@ -0,0 +1,70 @@
+#ifndef _SODIUM_PROMISE_H_
+#define _SODIUM_PROMISE_H_
+
+#include <sodium/sodium.h>
+#include <memory>
+
+namespace sodium {
+    template <class A>
+    class promise {
+    template <class B>
+    friend class promise;
+    private:
+        promise(
+            const stream<A>& sDeliver_,
+            const cell<boost::optional<A>>& oValue_)
+        : sDeliver(sDeliver_),
+          oValue(oValue_)
+        {
+        }
+    public:
+        promise()
+        : oValue(boost::optional<A>())
+        {
+        }
+        promise(const stream<A>& sDeliver_)
+        : sDeliver(sDeliver_.once()),
+          oValue(this->sDeliver.map<boost::optional<A>>([] (const A& a) {
+                  return boost::optional<A>(a);
+              }).hold(boost::optional<A>()))
+        {
+        }
+        stream<A> sDeliver;
+        cell<boost::optional<A>> oValue;
+
+        void then_do(const std::function<void(const A&)>& f) const {
+            transaction trans;
+            filter_optional(oValue.value()).listen_once(f);
+        }
+
+        template <typename Fn>
+        promise<typename std::result_of<Fn(A)>::type> map(Fn f) const {
+            typedef typename std::result_of<Fn(A)>::type B;
+            return promise<B>(
+                sDeliver.template map<B>(f),
+                oValue.template map<boost::optional<B>>(
+                    [f] (const boost::optional<A>& ob) {
+                        return ob ? boost::optional<B>(f(ob.get()))
+                                  : boost::optional<B>();
+                    }
+                ));
+        }
+    };
+
+    template <class A, class B, class C>
+    promise<C> lift(const std::function<C(const A&, const B&)>& f,
+        const promise<A>& pa, const promise<B>& pb)
+    {
+        transaction trans;
+        return promise<C>(
+            lift<boost::optional<A>, boost::optional<B>, boost::optional<C>>(
+                [f] (const boost::optional<A>& oa, const boost::optional<B>& ob) {
+                    return oa && ob ? boost::optional<C>(f(oa.get(), ob.get()))
+                                    : boost::optional<C>();
+                },
+                pa.oValue, pb.oValue
+            ));
+    }
+}
+
+#endif
---
+++ tests/memory/promise-memory.cpp
@@ -0,0 +1,24 @@
+/**
+ * Run with
+ * valgrind --leak-check=full memory/promise-memory
+ */
+#include <sodium/promise.h>
+#include <string>
+#include <iostream>
+
+void launch(sodium::stream_sink<std::string> s)
+{
+    sodium::promise<std::string> p(s);
+    /*
+    p.then_do([] (const std::string& text) {
+        std::cout << text << std::endl;
+    });
+    */
+}
+
+int main(int argc, char* argv[])
+{
+    sodium::stream_sink<std::string> s;
+    launch(s);
+    s.send("banana");
+}
